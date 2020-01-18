67c7f800bf220c70d99931c3ee9202052b3ea078
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-01-18 22:29:03 +0100

    added the note list to the home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-
     return null;
   }
 }
 
-class HomeSUI extends State<HomePage>{
+class HomeSUI extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-
-    return ;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notes'),
+      ),
+      body: getListView(context),
+    );
   }
 
+  Widget getListView(BuildContext context) {
+
+    return ListView.builder(itemBuilder: (context, index){
+      return ListTile(
+        leading: getPriorityIcon(index),
+        title: Text("Dummy title"),
+        trailing: Icon(Icons.delete),
+        onTap: (){
+          //todo
+        },
+      );
+    });
+  }
+
+  getPriorityIcon(int index) {}
 }
