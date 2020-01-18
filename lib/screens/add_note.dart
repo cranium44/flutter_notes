41f5fb5a5f145a972ab92c7aaa88ed276dd1e4dd
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-01-19 00:09:50 +0100

    added the add note screen basic scaffold

--- lib/screens/add_note.dart
+++ lib/screens/add_note.dart
@@ -1,2 +1,30 @@
 import 'package:flutter/material.dart';
 
+class AddNote extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return AddNoteUI();
+  }
+
+}
+
+class AddNoteUI extends State<AddNote>{
+
+  var _priorities = ["Low", "High"];
+  @override
+  Widget build(BuildContext context) {
+    return Material(child: Scaffold(
+      appBar: AppBar(
+        title: Text("Add Note"),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10),
+        children: <Widget>[
+
+        ],
+      ),
+    ),);
+  }
+
+}
